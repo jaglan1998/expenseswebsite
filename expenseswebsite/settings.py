49deb267023c9a73d5c9c2eb7410378c64dabffc
Balvinder Singh <bsinghjaglan@gmail.com>
2022-01-10 21:53:01 -0400

    Hide the Secret key with env

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m3l5(vv@ii2c1+n57n4nc0mpgtrd@_7_y)r^ihp^krpj$$i!$k'
+SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
